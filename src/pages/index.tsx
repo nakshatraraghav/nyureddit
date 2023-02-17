709907e2e882005fdf43e70cbb48096059d90c0a
nakshatra raghav <combatrickshaw@gmail.com>
2023-02-17 21:21:43 +0530

    configured and tested firebase + installed husky (pre-commit hook)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,58 @@
-import Head from "next/head";
+import { storage } from "@/firebase/app";
+import { ref, uploadString } from "firebase/storage";
 import Image from "next/image";
-import { Inter } from "@next/font/google";
-import styles from "@/styles/Home.module.css";
+import { ChangeEvent, useState } from "react";
 
-const inter = Inter({ subsets: ["latin"] });
+const Index = () => {
+  const [file, setFile] = useState<string>();
+  return (
+    <div className="flex items-center justify-center h-screen bg-black text-white">
+      {file ? (
+        <div>
+          <Image src={file} height={400} width={400} alt="image" />
+          <button
+            className="p-2 mr-4 bg-white rounded-lg text-black"
+            onClick={() => {
+              setFile("");
+            }}
+          >
+            remove
+          </button>
+          <button
+            className="p-2 bg-white rounded-lg text-black"
+            onClick={async () => {
+              const imageRef = ref(storage, "images/imgae.jpg");
+              try {
+                await uploadString(imageRef, file, "data_url");
+                alert("file uploaded");
+              } catch (error: any) {
+                alert(error.message);
+              }
+            }}
+          >
+            upload
+          </button>
+        </div>
+      ) : (
+        <div>
+          <input
+            type="file"
+            onChange={(evt: ChangeEvent<HTMLInputElement>) => {
+              const reader = new FileReader();
+              if (evt.target.files?.[0]) {
+                reader.readAsDataURL(evt.target.files[0]);
+              }
+              reader.onload = (readerEvent) => {
+                if (readerEvent.target?.result) {
+                  setFile(readerEvent.target.result as string);
+                }
+              };
+            }}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default function Home() {
-  return <div className="text-red-500 text-5xl">some text here</div>;
-}
+export default Index;
