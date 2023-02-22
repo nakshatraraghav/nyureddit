76053267b56dc3661d93aa9c5bbf5d27a536e35c
nakshatra raghav <combatrickshaw@gmail.com>
2023-02-22 10:41:59 +0530

    (ui): removed the uglyass borders from the page layout

--- src/layouts/PageLayout.tsx
+++ src/layouts/PageLayout.tsx
@@ -6,13 +6,13 @@ type PageLayoutProps = {
 
 const PageLayout: React.FC<PageLayoutProps> = ({ children }) => {
   return (
-    <div className="flex justify-center px-4 py-0 border border-red-900">
-      <div className="flex justify-center w-11/12 max-w-[860px] border border-green-900">
-        <div className="flex flex-col w-full md:w-4/6 mr-0 md:mr-6 border border-orange-600">
+    <div className="flex justify-center px-4 py-0">
+      <div className="flex justify-center w-11/12 max-w-[860px] border ">
+        <div className="flex flex-col w-full md:w-4/6 mr-0 md:mr-6 border">
           {children[0]}
         </div>
         {/*left container */}
-        <div className="hidden flex-grow md:flex md:flex-col border border-black">
+        <div className="hidden flex-grow md:flex md:flex-col border ">
           {children[1]}
         </div>{" "}
         {/*right container */}
